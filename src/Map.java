641c7b1fb3bc33fd462287eb73aa670cc4650f08
jang-jinuk <dlsndifn98@gmail.com>
2023-11-27 17:19:26 +0900

    add mapInformation method

--- src/Map.java
+++ src/Map.java
@@ -12,8 +12,10 @@ public class Map {
         System.out.printf("Stage%d\n\n",stage);
         stage++;
         printMap(stage1);
+        mapInformation(stage1);
         System.out.printf("Stage%d\n\n",stage);
         printMap(stage2);
+        mapInformation(stage2);
     }
     void printMap(int stage[][]){
         int rowLength = stage.length;
@@ -35,5 +37,33 @@ public class Map {
             System.out.println();
         }
         System.out.println();
+    }
+    void mapInformation(int[][] stage){
+        int width = 0;
+        width = stage[0].length;
+        int height = 0;
+        height = stage.length;
+
+        int hole = 0;
+        int ball = 0;
+        int[] playerPosition = new int[2];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (stage[i][j] == 1)
+                    hole++;
+                if (stage[i][j] == 2)
+                    ball++;
+                if (stage[i][j] == 3) {
+                    playerPosition[0] = i+1;
+                    playerPosition[1] = j+1;
+                }
+            }
+        }
+        System.out.printf("가로크기 : %d\n",width);
+        System.out.printf("세로크기 : %d\n",height);
+        System.out.printf("구멍의 수 : %d\n",hole);
+        System.out.printf("공의 수 : %d\n",ball);
+        System.out.printf("플레이어 위치 : %d행 %d열\n\n",playerPosition[0],playerPosition[1]);
     }
 }
